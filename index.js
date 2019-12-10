3ebe20f5db25b9bfae3bf8c8deb1577233d509c0
EnemiPrince <47088291+EnemiPrince@users.noreply.github.com>
2019-12-10 01:59:11 +0100

    init
    init

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ bot.on("ready", async () => {
 
     console.log(`**${bot.user.username}** je nyní online!`);
 
-    bot.user.setActivity("Ukazuje to klukům.", {type: "STREAMING"});
+    bot.user.setActivity("Ukazuje to klukům.", {type: "PLAYING"});
 
 });
 
